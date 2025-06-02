aab8b2f2ed3d01fd9961b59ddd354a305deaf698
sookan <digiusto.ugo@gmail.com>
2025-06-02 14:17:14 +0200

    wrong port

--- api/main.py
+++ api/main.py
@@ -75,4 +75,4 @@ async def del_npc(npc_id: int, should_redirect: bool = Depends(check_token)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
